dab63d0d2fd2ae437193035e2309f35f24676173
muhammed ozturk <muhammed.mhczr@gmail.com>
2023-11-06 19:54:05 +0300

    add reactstrap and bootstrap also add listgroup iitem for CategoryList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React from 'react';
 import CategoryList from './CategoryList';
 import ProductList from './ProductList';
 import Navigation from './Navigation';
+import { Container, Row, Col } from 'reactstrap';
 
 function App() {
   return (
     <div>
-      <Navigation/>
-      <CategoryList/>
-      <ProductList/>
+      <Container>
+        <Row>
+          <Navigation />
+
+        </Row>  
+        <Row>
+          <Col xs="3">
+            <CategoryList />
+
+          </Col>
+          <Col xs="9">
+            <ProductList />
+
+          </Col>
+
+        </Row>
+      </Container>
+
+
     </div>
   );
 }
