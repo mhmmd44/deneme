91595a5bf7c5fa41e8df61d4af57c9d5e841c929
muhammed ozturk <muhammed.mhczr@gmail.com>
2023-11-07 20:01:06 +0300

    using state in group item list

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem, ListGroupItemText } from 'reactstrap';
 
 export default class CategoryList extends Component {
- 
+
+  state = {
+    Categories: [
+    { categoryId: 1, categoryName: "Bavereges" },
+    { categoryId: 2, categoryName: "Condiments" }
+  ] 
+};
   render() {
     return (
       <div>
         <h2>{this.props.info.title}</h2>
         <ListGroup>
-        <ListGroupItem>Cras justo odio</ListGroupItem>
-        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-        <ListGroupItem>Morbi leo risus</ListGroupItem>
-        <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-        <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {
+            this.state.Categories.map(category=>(
+              <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+            ))}
         </ListGroup>
       </div>
     )
